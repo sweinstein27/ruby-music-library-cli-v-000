b8fa9a51718705a3908bb426d54d224408e366a9
Scott Weinstein <sweinstein27@gmail.com>
2018-06-08 14:42:00 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -19,10 +19,16 @@ class MusicLibraryController
     case input
     when "list songs"
       list_songs
-    when "list_artists"
+    when "list artists"
       list_artists
-    when "list_genres"
+    when "list genres"
       list_genres
+    when "list artist"
+      list_songs_by_artist
+    when "list genre"
+      list_songs_by_genre
+    when "play_song"
+      play_song
     end
 
 
